deac1ff5c9847027fdf2909a37ab0817abb2a49d
Lionel <lionelngaiwoh@yahoo.com>
2022-05-31 23:01:44 -0400

    added local

--- main.tf
+++ main.tf
@@ -14,6 +14,8 @@ locals {
     cell_name               = "WEB"
     component_name          = "kojitechs"
   }
+  azs = data.aws_availability_zones.azs.names
+
   vpc_id = aws_vpc.kojitechs_vpc.id
 
 }
@@ -52,7 +54,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "pub_subnet_1" {
   vpc_id     = local.vpc_id
   cidr_block = var.pub_subnet_cidr[0]
-  availability_zone = data.aws_availability_zones.azs.names[0]
+  availability_zone = local.azs[0]
   map_public_ip_on_launch = true
 
   tags = {
@@ -63,7 +65,7 @@ resource "aws_subnet" "pub_subnet_1" {
 resource "aws_subnet" "pub_subnet_2" {
   vpc_id     = local.vpc_id
   cidr_block = var.pub_subnet_cidr[1]
-  availability_zone = data.aws_availability_zones.azs.names[1]
+  availability_zone = local.azs[1]
   map_public_ip_on_launch = true
 
   tags = {
@@ -74,7 +76,7 @@ resource "aws_subnet" "pub_subnet_2" {
 resource "aws_subnet" "priv_subnet_1" {
   vpc_id     = local.vpc_id
   cidr_block = var.priv_subnet_cidr[0]
-  availability_zone = data.aws_availability_zones.azs.names[0]
+  availability_zone = local.azs[0]
 
   tags = {
     Name = "priv_subnet_${data.aws_availability_zones.azs.names[0]}" # RESULT priv_subnet_us-east-1a
@@ -84,7 +86,7 @@ resource "aws_subnet" "priv_subnet_1" {
 resource "aws_subnet" "priv_subnet_2" {
   vpc_id     = local.vpc_id
   cidr_block = var.priv_subnet_cidr[1]
-  availability_zone = data.aws_availability_zones.azs.names[1]
+  availability_zone = local.azs[1]
 
   tags = {
     Name = "priv_subnet_${data.aws_availability_zones.azs.names[1]}"
@@ -94,7 +96,7 @@ resource "aws_subnet" "priv_subnet_2" {
 resource "aws_subnet" "database_subnet_1" {
   vpc_id     = local.vpc_id
   cidr_block = var.database_subnet_cidr[0]
-  availability_zone = data.aws_availability_zones.azs.names[0]
+  availability_zone = local.azs[0]
 
   tags = {
     Name = "database_subnet_${data.aws_availability_zones.azs.names[0]}"
@@ -104,7 +106,7 @@ resource "aws_subnet" "database_subnet_1" {
 resource "aws_subnet" "database_subnet_2" {
   vpc_id     = local.vpc_id
   cidr_block = var.database_subnet_cidr[1]
-  availability_zone = data.aws_availability_zones.azs.names[1]
+  availability_zone = local.azs[1]
 
   tags = {
     Name = "database_subnet_${data.aws_availability_zones.azs.names[1]}"
